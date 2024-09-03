a8774b6952049cafb0169d538ce284aec84ef4f5
Alex Oliveira <alexoliveira7x@gmail.com>
2024-09-03 16:40:50 +0100

    rename path

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-import { VercelHandler } from '../handlers/vercel';
-import lambda from '../handlers/handler';
+import { VercelHandler } from '../src/handlers/vercel';
+import lambda from '../src/handlers/handler';
 
 export default async function handler(req, res) {
   lambda(req, res, VercelHandler);
