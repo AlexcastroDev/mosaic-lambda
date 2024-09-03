939b5d0f36c09696613f7dd1c87d5dbfe3f11524
Alex Oliveira <alexoliveira7x@gmail.com>
2024-09-03 16:43:16 +0100

    add extension

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-import { VercelHandler } from '../src/handlers/vercel';
-import lambda from '../src/handlers/handler';
+import { VercelHandler } from '../src/handlers/vercel.js';
+import lambda from '../src/handlers/handler.js';
 
 export default async function handler(req, res) {
   lambda(req, res, VercelHandler);
