744bbb9e98d8b0a8bfd0aea992e53b8bd343cb3e
Alex Oliveira <alexoliveira7x@gmail.com>
2024-09-05 17:48:10 +0100

    fix server path

--- aws-lambda/index.js
+++ aws-lambda/index.js
@@ -1,5 +1,5 @@
 import awsLambdaFastify from '@fastify/aws-lambda'
-import app from '../server.mjs'
+import app from './server.mjs'
 
 // https://github.com/fastify/aws-lambda-fastify
 export const handler = awsLambdaFastify(app)
