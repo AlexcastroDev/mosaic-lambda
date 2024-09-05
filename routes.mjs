ea4c1b598228ec7a8555f91e5b68825e54b90606
Alex Oliveira <alexoliveira7x@gmail.com>
2024-09-05 22:05:10 +0100

    update route

--- routes.mjs
+++ routes.mjs
@@ -4,4 +4,10 @@ export default function routes(fastify) {
   fastify.get('/', async function handler(request, reply) {
     return lambda(request, reply, FastifyHandler)
   })
+
+  // aws lambda
+  fastify.get('/mosaic-generator', async function handler(request, reply) {
+    return lambda(request, reply, FastifyHandler)
+  })
+
 }
